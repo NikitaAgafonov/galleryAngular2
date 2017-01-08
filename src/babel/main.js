9fa06fdceedce4ac6ae1ac4342afe71252caca63
NikitaAgafonov <overcome2014@mail.ru>
2017-01-08 14:04:38 +0700

    delete js files

--- src/babel/main.js
+++
@@ -1,5 +0,0 @@
-///<reference path="../../typings/index.d.ts"/>
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './module';
-platformBrowserDynamic().bootstrapModule(AppModule);
-//# sourceMappingURL=main.js.map
